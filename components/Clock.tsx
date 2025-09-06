70ce944f236dfa3bd03c1d2ac2027720554f608b
Jolomi Taiwo <taiwojolomi15@gmail.com>
2025-09-06 16:53:43 +0100

    - adjusted clock to show am or pm

--- components/Clock.tsx
+++ components/Clock.tsx
@@ -7,15 +7,24 @@ export default function Clock() {
 	const [time, setTime] = useState("");
 
 	useEffect(() => {
-		const interval = setInterval(() => {
-			setTime(new Date().toLocaleTimeString());
-		}, 1000);
+		const updateTime = () => {
+			setTime(
+				new Date().toLocaleTimeString([], {
+					hour: "2-digit",
+					minute: "2-digit",
+					hour12: true, // ensures AM/PM
+				})
+			);
+		};
+
+		updateTime(); // run immediately
+		const interval = setInterval(updateTime, 1000);
 
 		return () => clearInterval(interval);
 	}, []);
 
 	return (
-		<div className="text-[#FF5B1B] flex items-center justify-center gap-2">
+		<div className="text-[#FF5B1B] flex items-center justify-center gap-2 uppercase">
 			<Clock4 size={20} />
 			{time}
 		</div>
