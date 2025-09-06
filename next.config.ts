b4015fdab5567586e0d2d7472717cf3efe42ddb7
Jolomi Taiwo <taiwojolomi15@gmail.com>
2025-09-06 16:42:32 +0100

    - set destination route

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,15 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
 	/* config options here */
 	allowedDevOrigins: ["https://2fce8dab6fa1.ngrok-free.app"],
+	async redirects() {
+		return [
+			{
+				source: "/",
+				destination: "/home",
+				permanent: false,
+			},
+		];
+	},
 };
 
 export default nextConfig;
